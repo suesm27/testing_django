4b7c642e8a7a8443a04953c7160f23ad90e36015
suesm27 <suesm27@gmail.com>
2015-11-11 23:43:35 -0500

    customized admin page

--- polls/models.py
+++ polls/models.py
@@ -1,5 +1,6 @@
 from django.db import models
-
+from django.utils import timezone
+import datetime
 
 class Question(models.Model):
     question_text = models.CharField(max_length=200)
@@ -8,6 +9,9 @@ class Question(models.Model):
     	return self.question_text
     def was_published_recently(self):
     	return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+	was_published_recently.admin_order_field = 'pub_date'
+	was_published_recently.boolean = True
+	was_published_recently.short_description = 'Published recently?'
 
 
 class Choice(models.Model):
